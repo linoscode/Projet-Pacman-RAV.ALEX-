14e7edea586232d8848b2ef81e7e724e67b63769
RoiVaka <67166114+RoiVaka@users.noreply.github.com>
2020-10-31 20:33:44 +0100

    Update comp_jeu.c

--- src/comp_jeu.c
+++ src/comp_jeu.c
@@ -10,7 +10,6 @@ Pos deplacer_pacman(Partie p,char **plateau,int direction) {
 	plateau[p.pacman.l][p.pacman.c]=' ';//On efface pacman de son ancien emplacement
 	//On dessine pacman dans la direction donné
 	if(direction==haut) {
-		printf("Vers le haut ! \n");
 		p.pacman.l--;
 		plateau[p.pacman.l][p.pacman.c]='P';
 		return p.pacman;
