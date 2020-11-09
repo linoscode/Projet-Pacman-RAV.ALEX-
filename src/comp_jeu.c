b4ad626880e87b588d78dded8acca362814e652b
RoiVaka <ralamboarivony.ravaka@gmail.com>
2020-11-09 17:10:43 +0100

    correction bug ou le pacman disparait lorsqu'il rencontre un mur

--- src/comp_jeu.c
+++ src/comp_jeu.c
@@ -7,21 +7,25 @@
 
 
 Pos deplacer_pacman(Partie p,char **plateau,int direction) {
-	plateau[p.pacman.l][p.pacman.c]=' ';//On efface pacman de son ancien emplacement
+  //On efface pacman de son ancien emplacement
 	//On dessine pacman dans la direction donné
 	if(direction==haut && plateau[p.pacman.l-1][p.pacman.c]!='*') {
+		plateau[p.pacman.l][p.pacman.c]=' ';
 		p.pacman.l--;
 		plateau[p.pacman.l][p.pacman.c]='P';
 		return p.pacman;
 	} else if (direction==droite && plateau[p.pacman.l][p.pacman.c+1]!='*') {
+		plateau[p.pacman.l][p.pacman.c]=' ';
 		p.pacman.c++;
 		plateau[p.pacman.l][p.pacman.c]='P';
 		return p.pacman;
 	} else if (direction==bas && plateau[p.pacman.l+1][p.pacman.c]!='*') {
+		plateau[p.pacman.l][p.pacman.c]=' ';
 		p.pacman.l++;
 		plateau[p.pacman.l][p.pacman.c]='P';
 		return p.pacman;
 	} else if (direction==gauche && plateau[p.pacman.l][p.pacman.c-1]!='*') {
+		plateau[p.pacman.l][p.pacman.c]=' ';
 		p.pacman.c--;
 		plateau[p.pacman.l][p.pacman.c]='P';
 		return p.pacman;
