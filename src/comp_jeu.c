a2ae2d7d018488e2fe901cdcd590582029646381
RoiVaka <ralamboarivony.ravaka@gmail.com>
2020-11-16 15:39:41 +0100

    ajout de commentaires

--- src/comp_jeu.c
+++ src/comp_jeu.c
@@ -1,24 +1,36 @@
 #include "./comp_jeu.h"
 
+/***********************************/
+/* Fonctions visuelles             */
+/***********************************/
+
 Pos deplacer_pacman_visuel(Partie p, char **plateau, int direction, int taille) {
+
 	Point pacman;
 	Point pacman_cible;
+
 	printf("( %d , %d ) \n", p.pacman.c, p.pacman.l);
+	//à remplacer par pos2coin
 	pacman.x = p.pacman.c*taille - p.pacman.c%taille;
 	pacman.y = p.pacman.l*taille - p.pacman.l%taille;
+
 	p.pacman = deplacer_pacman_plateau(p,plateau,direction);
 	printf("( %d , %d ) \n", p.pacman.c, p.pacman.l);
+	//à remplacer par pos2coin
 	pacman_cible.x = p.pacman.c*taille - p.pacman.c%taille;
 	pacman_cible.y = p.pacman.l*taille - p.pacman.l%taille;
 
 	if(direction == haut || direction == bas)
 	{
+		//On déplace pacman tant qu'il n'a pas atteint les coordonnées de la case suivante
 		while(pacman.y != pacman_cible.y)
 		{
 			dessiner_disque(pacman, taille, noir);
+			//Utilise selon le vecteur (0;-1) ou (0;1) selon la direction choisie.
 			pacman.y += (direction == haut) ? -1 : 1;
 			dessiner_disque(pacman, taille, jaune);
 			actualiser();
+			//On attend la fréquence
 			attente(FREQ);
 		}
 	}
@@ -28,11 +40,14 @@ Pos deplacer_pacman_visuel(Partie p, char **plateau, int direction, int taille) 
 		while(pacman.x != pacman_cible.x)
 		{
 			dessiner_disque(pacman, taille, noir);
+			//Utilise selon le vecteur (1;0) ou (-1;0) selon la direction choisie.
 			pacman.x += (direction == droite) ? 1 : -1;
-			if (pacman.x < 0) {
+			if (pacman.x < 0)
+			{
 				pacman.x = p.C*taille - p.C%taille;
 			}
-			if (pacman.x > p.C*taille - p.C%taille ) {
+			if (pacman.x > p.C*taille - p.C%taille )
+			{
 				pacman.x = 0;
 			}
 			dessiner_disque(pacman, taille, jaune);
@@ -42,6 +57,10 @@ Pos deplacer_pacman_visuel(Partie p, char **plateau, int direction, int taille) 
 	}
 	return p.pacman;
 }
+
+/******************************************/
+/* Fonctions modélisant les règles du jeu */
+/******************************************/
 
 
 Pos deplacer_pacman_plateau(Partie p,char **plateau,int direction) {
@@ -58,12 +77,15 @@ Pos deplacer_pacman_plateau(Partie p,char **plateau,int direction) {
 	else if (direction==droite && p.plateau[p.pacman.l][p.pacman.c+1]!='*')
 	{
 		plateau[p.pacman.l][p.pacman.c]=' ';
+		//Si pacman sort du plateau on le ramène de l'autre côté
+		//permet de créer des tunnels en laissant des trous vers l'extérieur du plateau
 		if(p.pacman.c==p.C-1)
 		{
 			p.pacman.c=0;
 			plateau[p.pacman.l][p.pacman.c]='P';
 		}
-		else {
+		else
+		{
 			p.pacman.c++;
 			plateau[p.pacman.l][p.pacman.c]='P';
 		}
@@ -86,10 +108,12 @@ Pos deplacer_pacman_plateau(Partie p,char **plateau,int direction) {
 			p.pacman.c=p.C-1;
 			plateau[p.pacman.l][p.pacman.c]='P';
 		}
-		else {
+		else
+		{
 			p.pacman.c--;
 			plateau[p.pacman.l][p.pacman.c]='P';
 		}
+
 		return p.pacman;
 	}
 
@@ -97,6 +121,7 @@ Pos deplacer_pacman_plateau(Partie p,char **plateau,int direction) {
 
 }
 
+//Fonction qui permet de compter le nombre de pacgommes sur le plateau
 int nbpacgommes(Partie p) {
 	for(int i=0; i<p.L;i++)
 	{
@@ -112,7 +137,10 @@ int nbpacgommes(Partie p) {
 }
 
 
-//Pour debuguer
+/******************************************/
+/* Fonctions de déboggage                 */
+/******************************************/
+
 void afficher_plan(Partie p) {
 	for(int i=0; i<p.L;i++) {
 		for(int j=0;j<p.C;j++) {
