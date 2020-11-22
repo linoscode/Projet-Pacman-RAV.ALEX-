8934c856ff804003aa77cbe5169c399bbdbb72a9
linoscode <73476883+linoscode@users.noreply.github.com>
2020-11-22 04:34:43 +0100

    Chargement 3 niveaux

--- src/main.c
+++ src/main.c
@@ -16,48 +16,52 @@ int main(int argc, char **argv)
     int i,j;
 
     /** Traitement des arguments **************************************************/
-    if(argc!=2) // Si le nombre d'arguments est différent de 2
+    if(argc!=4) // Si le nombre d'arguments est différent de 2
         {
-        printf("Usage: %s fichier_plan\n", argv[0]); // mode d'emploi et sortie
+        printf("Usage: %s + 3 fichiers plans\n", argv[0]); // mode d'emploi et sortie
         return 0;
         }
     /******************************************************************************/
+   
+   /*Boucle pour charger les niveaux une fois les pacgums mangés */
+   for(int niveau = 1 ; niveau < argc ; niveau++)
+   {
+       /* Chargement du plan à partir du fichier fourni en paramètre                 */
+       printf("Chargement du plan...\n");
+       p = charge_plan(argv[niveau]);
+       /* Si problème lors du chargement du plan...                                  */
+       if(p.plateau == NULL)
+           return 1;
 
-    /* Chargement du plan à partir du fichier fourni en paramètre                 */
-    printf("Chargement du plan...\n");
-    p = charge_plan(argv[1]);
-    /* Si problème lors du chargement du plan...                                  */
-    if(p.plateau == NULL)
-        return 1;
+   #if DEBUG==1 //Ceci est un drapeau à activer/désactiver dans main.h
+       /* Affichage du plan lu                                                       */
+       printf("Affichage du plan...\n");
+       for(i=0;i!=p.L;i++)
+           {
+           for(j=0;j!=p.C;j++)
+               printf("%c",p.plateau[i][j]);
+           printf("\n");
+           }
+   #endif
+      
+      /* Déclaration des variables*/
+      int game_over;
+      
+      //Démarrage du jeu.
+      ouvrir_fenetre(800,600);
+      demarrage();
 
-#if DEBUG==1 //Ceci est un drapeau à activer/désactiver dans main.h
-    /* Affichage du plan lu                                                       */
-    printf("Affichage du plan...\n");
-    for(i=0;i!=p.L;i++)
-        {
-        for(j=0;j!=p.C;j++)
-            printf("%c",p.plateau[i][j]);
-        printf("\n");
-        }
-#endif
-
-/******************************************************************************/
-/* A PARTIR D'ICI...C'EST A VOUS DE JOUER!                                    */
-/******************************************************************************/
-/* Déclaration des variables*/
-int game_over;
-// Démarrage du jeu.
-    ouvrir_fenetre(800,600);
-    demarrage();
-
-    // Affichage du plateau.
-    game_over = demarrer_partie(p);
-    if (game_over) {
-      printf("Vous avez perdu !\n");
-      fermer_fenetre();
-    }
-    // Fermeture du jeu.
-    attendre_clic();
-    fermer_fenetre();
-    return 0;
-    }
+       // Partie commence
+       game_over = demarrer_partie(p);
+      
+      //Si défaite :
+       if (game_over) {
+         printf("Vous avez perdu !\n");
+         fermer_fenetre();
+       }
+      
+// Fermeture du jeu.
+attendre_clic();
+fermer_fenetre();
+return 0;
+}
